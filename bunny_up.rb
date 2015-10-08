ff7f3fdc9c926ec149e557afe13160647d616472
Iwona Sztorc <ivsztorc@gmail.com>
2015-10-08 12:01:53 +0100

    Remove extra spaces

--- bunny_up.rb
+++ bunny_up.rb
@@ -1,36 +1,36 @@
 def group_size_calc
-  students = %w(Iwona Craig GraemeK Kathryn Kieran Peter Ere Chae Zsolt Simon Syed Neil GraemeS Keith Nevin)
+  students = %w(Iwona Craig GraemeK Kathryn Kieran Peter Ere Chae Zsolt Simon Syed Neil GraemeS Keith Nevin)
 
-  puts `clear`
-  puts "Bunny Up (what?)"
-  puts "How many people do you wish to assign to a group?"
-  # get user input
-  group_size = gets.chomp.to_i
-  # store number of groups
-  number = students.size / group_size
-  # calculate remaining students
-  remainder = students.size % group_size
-  # store random remaining students
-  remaining_students = students.sample(remainder)
+  puts `clear`
+  puts "Bunny Up (what?)"
+  puts "How many people do you wish to assign to a group?"
+  # get user input
+  group_size = gets.chomp.to_i
+  # store number of groups
+  number = students.size / group_size
+  # calculate remaining students
+  remainder = students.size % group_size
+  # store random remaining students
+  remaining_students = students.sample(remainder)
 
-  remaining_students.each { |student| students.delete student}
+  remaining_students.each { |student| students.delete student}
 
-  groups = []
-  number.times do
-    new_group = students.sample(group_size)
-    groups << new_group
-    new_group.each { |student| students.delete student}
-  end
+  groups = []
+  number.times do
+    new_group = students.sample(group_size)
+    groups << new_group
+    new_group.each { |student| students.delete student}
+  end
 
-  remaining_students.each_with_index do |s, i|
-    groups[i] << s
-  end
+  remaining_students.each_with_index do |s, i|
+    groups[i] << s
+  end
 
-  groups.each_with_index do |group, index|
-    puts "Group #{index + 1}: "
-    group.each { |name| print name + " "}
-    puts
-  end
+  groups.each_with_index do |group, index|
+    puts "Group #{index + 1}: "
+    group.each { |name| print name + " "}
+    puts
+  end
 
 end
 
